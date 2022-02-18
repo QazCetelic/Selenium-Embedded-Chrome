8cc457da64367c5941634d4f23d1aa41ded18735
QazCetelic <qaz.cetelic@protonmail.com>
2022-02-18 08:11:48 +0100

    Bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,13 +15,13 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test:1.6.0")
 
     // TODO test what is unnecessary and remove it
-    implementation("org.seleniumhq.selenium:selenium-java:4.1.1")
-    implementation("org.seleniumhq.selenium:selenium-api:4.1.1")
+    implementation("org.seleniumhq.selenium:selenium-java:4.1.2")
+    implementation("org.seleniumhq.selenium:selenium-api:4.1.2")
     implementation("org.seleniumhq.selenium:selenium-server:3.141.59")
-    implementation("org.seleniumhq.selenium:selenium-chrome-driver:4.1.1")
+    implementation("org.seleniumhq.selenium:selenium-chrome-driver:4.1.2")
 
-    implementation("org.slf4j:slf4j-api:1.7.33")
-    implementation("org.slf4j:slf4j-simple:1.7.33")
+    implementation("org.slf4j:slf4j-api:1.7.35")
+    implementation("org.slf4j:slf4j-simple:1.7.36")
 
     implementation("net.lingala.zip4j:zip4j:2.9.1")
 }
